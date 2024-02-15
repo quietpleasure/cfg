e5eff567e1753f0cc85fb7d61325fe2cfa7eab96
Triod Android <triod_android@inbox.ru>
2024-02-15 18:27:47 +0200

    -

--- cfg.go
+++ cfg.go
@@ -65,8 +65,8 @@ func (c *Config) Decode(out interface{}, tagname ...string) error {
 			DecodeHook: mapstructure.ComposeDecodeHookFunc(
 				mapstructure.StringToTimeDurationHookFunc(),
 			),
-			ErrorUnused: true,
-			ErrorUnset:  true,
+			// ErrorUnused: true,
+			// ErrorUnset:  true,
 		},
 	)
 	if err != nil {
