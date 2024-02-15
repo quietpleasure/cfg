0129eef97541e0a4d728243f41652a65879c9c4c
Andrei Sorokin <nuclear_louse@mail.ru>
2024-02-15 17:47:04 +0200

    init

---
+++ cfg.go
@@ -0,0 +1,69 @@
+package cfg
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type Config struct {
+	Data map[string]interface{}
+}
+
+func New() *Config {
+	return &Config{
+		Data: make(map[string]interface{}),
+	}
+}
+
+func (c *Config) LoadGlob(pattern string) error {
+	files, err := filepath.Glob(pattern)
+	if err != nil {
+		return err
+	}
+	for _, filename := range files {
+		err = c.LoadFile(filename)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//TODO: add .ini
+func (c *Config) LoadFile(filename string) error {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	switch filepath.Ext(filename) {
+	case ".yml", ".yaml":
+		return c.LoadYAML(data)
+	case ".json":
+		return c.LoadJSON(data)
+	case ".toml":
+		return c.LoadTOML(data)
+	}
+	return nil
+}
+
+// Default tag=cfg
+func (c *Config) Decode(out interface{}, tagname ...string) error {
+	tag := "cfg"
+	if tagname != nil {
+		tag = tagname[0]
+	}
+	d, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		TagName:          tag,
+		WeaklyTypedInput: true,
+		Result:           out,
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeDurationHookFunc(),
+		),
+	})
+	if err != nil {
+		return err
+	}
+	return d.Decode(c.Data)
+}
---
+++ json.go
@@ -0,0 +1,19 @@
+package cfg
+
+import (
+	"encoding/json"
+
+	"dario.cat/mergo"
+)
+
+func (c *Config) LoadJSONString(s string) error {
+	return c.LoadJSON([]byte(s))
+}
+
+func (c *Config) LoadJSON(data []byte) error {
+	res := make(map[string]interface{})
+	if err := json.Unmarshal(data, &res); err != nil {
+		return err
+	}
+	return mergo.Merge(&c.Data, res, mergo.WithOverride)
+}
